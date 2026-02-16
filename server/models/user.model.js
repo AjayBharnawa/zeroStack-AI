9fda42b9ba57e4ec514c9c8410ea474b7028a3b3
Ajay <bharnawaajay246@gmail.com>
2026-02-16 11:18:38 +0530

    Initial commit: setup backend

---
+++ server/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    avatar:{
+        type:String
+    },
+    credits:{
+        type:number,
+        default:100,
+        min:0
+    },
+    plan:{
+        type:String,
+        enum:["free", "pro", "enterprise"],
+        default: "free"
+    }
+},{timestramp:true})
+
+const User=mongoose.model("USer",userSchema)
+export default User 
